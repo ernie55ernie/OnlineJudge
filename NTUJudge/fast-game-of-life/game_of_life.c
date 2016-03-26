f644f2ba74a4a5b943a414cae97cc479806a752f
Yu-Wei_Chang <ernie55ernie@yahoo.com.tw>
2016-03-26 17:55:06 +0800

    Game of life

--- NTUJudge/fast-game-of-life/game_of_life.c
+++ NTUJudge/fast-game-of-life/game_of_life.c
@@ -1,81 +1,69 @@
 #include <stdio.h>
+#include <string.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int ok(int plate[], int n, int index, int adj){
-	if(adj < 0 || adj >= n * n)
-		return 0;
-	if(index % n == 0 && index == (adj + 1))
-		return 0;
-	if(index % n == (n - 1) && index == (adj - 1))
-		return 0;
-	return 1;
+#define MAX_N 2000
+
+int plate[2][(MAX_N + 2) * (MAX_N + 2)];
+int which = 0;
+int n;
+
+int live(int index){
+	return (plate[which][index - n - 3] 
+		+ plate[which][index - n - 2]
+		+ plate[which][index - n - 1]
+		+ plate[which][index - 1]
+		+ plate[which][index + 1]
+		+ plate[which][index + n + 1]
+		+ plate[which][index + n + 2]
+		+ plate[which][index + n + 3]);
 }
 
-int live(int plate[], int n, int index){
-	int count = 0;
-	for(int i = -1; i < 2; i++){
-		for(int j = -1; j < 2; j++){
-			if(i == 0 && j == 0)
-				continue;
-			int adj = index + j + i * n;
-			if(ok(plate, n, index, adj) &&
-				plate[adj])
-				count++;
-		}
-	}
-	return count;
-}
-
-void place_all(int plate[], int new[], int n){
-#pragma omp parallel for
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			plate[i * n + j] = new[i * n + j];
-		}
-	}
-}
-
-void iteration(int plate[], int n){
-	int new[n * n];
-#pragma omp parallel for
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			int index = i * n + j;
-			int num = live(plate, n, index);
-			if(plate[index]){
-				new[index] =  (num == 2 || num == 3) ?
+void iteration(){
+#pragma omp parallel for schedule(static)
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
+			int index = i * (n + 2) + j;
+			int num = live(index);
+			if(plate[which][index]){
+				plate[!which][index] =  (num == 2 || num == 3) ?
 					1 : 0;
 			}else{
-				new[index] = (num == 3);
+				plate[!which][index] = (num == 3);
 			}
 		}
 	}
-	place_all(plate, new, n);
+	which = !which;
 }
 
-void print_plate(int plate[], int n){
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			printf("%d", plate[i * n + j]);
+void print_plate(){
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
+			printf("%d", plate[which][i * (n + 2) + j]);
 		}
 		printf("\n");
 	}
+	printf("\0");
 }
 
 int main(){
-	int N, M;
-	scanf("%d %d\n", &N, &M);
-	int plate[N * N];
-	for(int i = 0; i < N; i++){
-		char line[N];
-		scanf("%s", &line);
-		for(int j = 0; j < N; j++){
-			plate[i * N + j] = line[j] - '0';
+	int M;
+	char line[MAX_N];
+	memset(plate[0], 0, sizeof(int) * (n + 2) * (n + 2));
+	memset(plate[1], 0, sizeof(int) * (n + 2) * (n + 2));
+	if(scanf("%d %d", &n, &M) == 2){
+		for(int i = 1; i <= n; i++){
+			scanf("%s", &line);
+			for(int j = 0; j < n; j++){
+				plate[0][i * (n + 2) + j + 1] = line[j] - '0';
+			}
 		}
-		scanf("\n");
+		for(int i = 0; i < M; i++){
+			iteration();
+		}
+		print_plate();
 	}
-	for(int i = 0; i < M; i++){
-		iteration(plate, N);
-	}
-	print_plate(plate, N);
 	return 0;
 }
