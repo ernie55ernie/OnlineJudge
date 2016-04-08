d4914dc50e1ed63940a8da12921c123fe1835d5f
Yu-Wei_Chang <ernie55ernie@yahoo.com.tw>
2016-04-08 12:50:16 +0800

    Change to BFS

--- NTUJudge/fast-sudoku/sudoku.c
+++ NTUJudge/fast-sudoku/sudoku.c
@@ -2,33 +2,35 @@
 
 #define N 9
 
-int placeNumber(int n, int border[N][N]){
+void makeConflict(int conflict[N][N][N + 1], int row, int col, int num, int value){
+	int block_row = row / 3;
+	int block_col = col / 3;
+	for(int i = 0; i < N; i++){
+		conflict[row][i][num] = value;
+		conflict[i][col][num] = value;
+	}
+	for(int i = 0; i < 3; i++)
+		for(int j = 0; j < 3; j++)
+			conflict[3 * block_row + i][3 * block_col + j][num] = value;
+}
+
+int placeNumber(int n, int border[N][N], int conflict[N][N][N + 1]){
 	if(n == N * N)
 		return 1;
 	int row = n / N;
 	int col = n % N;
 	if(border[row][col] != 0)
-		return placeNumber(n + 1, border);
-	int block_row = row / 3;
-	int block_col = col / 3;
+		return placeNumber(n + 1, border, conflict);
 	int numSolution = 0;
 	for(int try = 1; try < N + 1; try++){
-		int conflict = 0;
-		for(int i = 0; i < N && !conflict; i++){
-			if(((col != i) && (border[row][i] == try)) || ((row != i) && (border[i][col] == try)))
-				conflict = 1;
+		//printf("row:%d, col:%d, try:%d, conflict[row][col][try]:%d\n", row, col, try, conflict[row][col][try]);
+		if(conflict[row][col][try]){
+			continue;
 		}
-		if(!conflict){
-			for(int i = 0; i < 3 && !conflict; i++)
-				for(int j = 0; j < 3 && !conflict; j++)
-					if(border[3 * block_row + i][3 * block_col + j] == try
-						&& (3 * block_row + i != row || 3 * block_col + j != col))
-						conflict = 1;
-			if(!conflict){
-				border[row][col] = try;
-				numSolution += placeNumber(n + 1, border);
-			}
-		}
+		makeConflict(conflict, row, col, try, 1);
+		border[row][col] = try;
+		numSolution += placeNumber(n + 1, border, conflict);
+		makeConflict(conflict, row, col, try, 0);
 	}
 	border[row][col] = 0;
 	return numSolution;
@@ -37,11 +39,15 @@ int placeNumber(int n, int border[N][N]){
 int main(){
 	int firstZero = -1;
 	int border[N][N];
+	int conflict[N][N][N + 1] = {0};
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < N; j++){
 			scanf("%d", &border[i][j]);
 			if(border[i][j] == 0 && firstZero == -1)
 				firstZero = i * N + j;
+			if(border[i][j]){
+				makeConflict(conflict, i, j, border[i][j], 1);
+			}
 		}
 	}
 	int numSolution = 0;
@@ -49,24 +55,16 @@ int main(){
 	int col = firstZero % N;
 	int block_row = row / 3;
 	int block_col = col / 3;
-#pragma omp parallel for reduction(+ : numSolution) firstprivate(border) schedule(dynamic)
+#pragma omp parallel for reduction(+ : numSolution) firstprivate(border, conflict) schedule(dynamic)
 	for(int try = 1; try < N + 1; try++){
-		int conflict = 0;
+		//printf("row:%d, col:%d, try:%d, conflict[row][col][try]:%d\n", row, col, try, conflict[row][col][try]);
+		if(conflict[row][col][try]){
+			continue;
+		}
+		makeConflict(conflict, row, col, try, 1);
 		border[row][col] = try;
-		for(int i = 0; i < N && !conflict; i++){
-			if(((col != i) && (border[row][i] == try)) || ((row != i) && (border[i][col] == try)))
-				conflict = 1;
-		}
-		if(!conflict){
-			for(int i = 0; i < 3 && !conflict; i++)
-				for(int j = 0; j < 3 && !conflict; j++)
-					if(border[3 * block_row + i][3 * block_col + j] == try
-						&& (3 * block_row + i != row || 3 * block_col + j != col))
-						conflict = 1;
-		}
-		if(!conflict){
-			numSolution += placeNumber(firstZero + 1, border);	
-		}
+		numSolution += placeNumber(firstZero + 1, border, conflict);
+		makeConflict(conflict, row, col, try, 0);
 	}
 	printf("%d\n", numSolution);
 	return 0;
