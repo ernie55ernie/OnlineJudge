4fd9598fe990ce9c48bc21e44ef789a23e0ca5f3
Yu-Wei_Chang <ernie55ernie@yahoo.com.tw>
2016-04-11 21:15:25 +0800

    Fast n queen with obstaclse

--- NTUJudge/fast-matrix-multiplication-pthread/matrix.c
+++ NTUJudge/fast-matrix-multiplication-pthread/matrix.c
@@ -5,28 +5,45 @@
 
 int NG;
 unsigned long (*AG)[2048];
+unsigned long (*BG)[2048];
 unsigned long (*CG)[2048];
 unsigned long BT[2048][2048];
 
 typedef struct arguments{
     int starti;
     int endi;
+    int startj;
+    int endj;
 }arguments;
 
 void* compute_row(void* args){
     arguments* argument = (arguments*)args;
     int starti = argument->starti;
     int endi = argument->endi;
-    printf("%d %d\n", starti, endi);
-    unsigned long sum;    // overflow, let it go.
-    for(int i = starti; i < endi; i++){
+    int startj = argument->startj;
+    int endj = argument->endj;
+    for (int j = startj; j < NG; j++) {
+        unsigned long sum = 0;    // overflow, let it go.
+        for (int k = 0; k < NG; k++)
+            sum += AG[starti][k] * BT[j][k];
+            // sum += AG[starti][k] * BG[k][j];
+        CG[starti][j] = sum;
+    }
+    for(int i = starti + 1; i < endi - 1; i++){
         for (int j = 0; j < NG; j++) {
-            sum = 0;
+            unsigned long sum = 0;    // overflow, let it go.
             for (int k = 0; k < NG; k++)
                 sum += AG[i][k] * BT[j][k];
                 // sum += AG[i][k] * BG[k][i];
             CG[i][j] = sum;
         }
+    }
+    for (int j = 0; j < endj; j++) {
+        unsigned long sum = 0;    // overflow, let it go.
+        for (int k = 0; k < NG; k++)
+            sum += AG[endi - 1][k] * BT[j][k];
+            // sum += AG[endi - 1][k] * BG[k][j];
+        CG[endi - 1][j] = sum;
     }
     pthread_exit(NULL);
 }
@@ -34,6 +51,7 @@ void* compute_row(void* args){
 void multiply(int N, unsigned long A[][2048], unsigned long B[][2048], unsigned long C[][2048]) {
     NG = N;
     AG = A;
+    BG = B;
     CG = C;
 
     pthread_attr_t attr;
@@ -45,18 +63,18 @@ void multiply(int N, unsigned long A[][2048], unsigned long B[][2048], unsigned 
 
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            BT[j][i] = B[i][j];
+            BT[j][i] = BG[i][j];
         }
     }
-    
+
     int dis = N * N < NUM_T ? N * N : NUM_T;
     int part = (N * N < NUM_T ? 1 : N * N / NUM_T);
-    int part_row = part / N;
-    printf("%d %d %d\n", N * N, dis, part);
     for (int i = 0; i < dis; i++) {
         arguments args;
-        args.starti = i == 0 ? i * part_row : i * part_row + 1;
-        args.endi =  i == dis - 1 ? N : ((i + 1) * part_row + 1);
+        args.starti = i * part / N;
+        args.endi =  i == dis - 1 ? N : (i + 1) * part / N == args.starti ? args.starti + 1 : (i + 1) * part / N;
+        args.startj = i * part % N;
+        args.endj = (i + 1) * part % N == 0 ? N : (i + 1) * part % N;
         args_pool[i] = args;
         pthread_create(&thread_pool[i], &attr, compute_row, (void*) &args_pool[i]);
     }
---
+++ NTUJudge/fast-n-queen/queen.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_N 20
+int n;
+int limit[MAX_N][MAX_N];
+
+void print_board(){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			printf("%c", limit[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int ok(int position[], int next, int test){
+	if(limit[next][test] == '*'){
+		return 0;
+	}
+	for(int i = 0; i < next; i++){
+		if(position[i] == test || (abs(test - position[i]) == next - i)){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int queen(int position[], int next){
+	if(next >= n)
+		return 1;
+	int sum = 0;
+	for(int test = 0; test < n; test++){
+		if(ok(position, next, test)){
+			position[next] = test;
+			sum += queen(position, next + 1);
+		}
+	}
+	return sum;
+}
+
+int main(){
+	int case_num = 0;
+	while(scanf("%d", &n) == 1){
+		int position[n];
+		int numSolution = 0;
+		char line[n];
+		for(int i = 0; i < n; i++){
+			scanf("%s", &line);
+			for(int j = 0; j < n; j++){
+				limit[i][j] = line[j];
+			}
+		}
+#pragma omp parallel for private(position) reduction(+ : numSolution) schedule(dynamic)
+		for(int test = 0; test < n; test++){
+			if(ok(position, 0, test)){
+				position[0] = test;
+				numSolution += queen(position, 1);
+			}
+		}
+		printf("Case %d: %d\n", ++case_num, numSolution);
+	}
+	return 0;
+}
