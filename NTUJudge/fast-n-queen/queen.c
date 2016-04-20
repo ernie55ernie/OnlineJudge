700e339253b9053227727d5a90afebee788a2135
Yu-Wei Chang <b01505025@ntu.edu.tw>
2016-04-20 20:02:59 +0800

    Modified queen

--- NTUJudge/fast-n-queen/queen.c
+++ NTUJudge/fast-n-queen/queen.c
@@ -3,37 +3,36 @@
 
 #define MAX_N 20
 int n;
-int limit[MAX_N][MAX_N];
 
-void print_board(){
+int print_limit(int limit[MAX_N][MAX_N], int n){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			printf("%c", limit[i][j]);
+			printf("%d ", limit[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-int ok(int position[], int next, int test){
-	if(limit[next][test] == '*'){
-		return 0;
-	}
-	for(int i = 0; i < next; i++){
-		if(position[i] == test || (abs(test - position[i]) == next - i)){
-			return 0;
+int set_limit(int limit[MAX_N][MAX_N], int col, int put, int value){
+	for(int i = 0; i < n; i++){
+		if(i != col){
+			limit[i][put] += value;
+			limit[col][i] += value;
+			if(put + i - col < n && put + i - col >= 0)limit[i][put + i - col] += value;
+			if(put - i + col >= 0 && put - i + col < n)limit[i][put - i + col] += value;
 		}
 	}
-	return 1;
 }
 
-int queen(int position[], int next){
+int queen(int limit[MAX_N][MAX_N], int next){
 	if(next >= n)
 		return 1;
 	int sum = 0;
 	for(int test = 0; test < n; test++){
-		if(ok(position, next, test)){
-			position[next] = test;
-			sum += queen(position, next + 1);
+		if(!limit[next][test]){
+			set_limit(limit, next, test, 1);
+			sum += queen(limit, next + 1);
+			set_limit(limit, next, test, -1);
 		}
 	}
 	return sum;
@@ -41,31 +40,36 @@ int queen(int position[], int next){
 
 int main(){
 	int case_num = 0;
+	int limit[MAX_N][MAX_N] = {0};
 	while(scanf("%d", &n) == 1){
-		int position[n];
 		int numSolution = 0;
 		char line[n];
 		for(int i = 0; i < n; i++){
 			scanf("%s", &line);
 			for(int j = 0; j < n; j++){
-				limit[i][j] = line[j];
+				limit[i][j] += line[j] == '*' ? 1 : 0;
 			}
 		}
-#pragma omp parallel for private(position) reduction(+ : numSolution) schedule(dynamic)
+		int printed = 0;
+#pragma omp parallel for private(limit) reduction(+ : numSolution) schedule(dynamic)
 		for(int test = 0; test < n * n * n * n; test++){
-			if(ok(position, 0, test / n / n / n)){
-				position[0] = test / n / n / n;
-				if(ok(position, 1, test / n / n % n)){
-					position[1] = test / n / n % n;
-					if(ok(position, 2, test / n % n)){
-						position[2] = test / n % n;
-						if(ok(position, 3, test % n)){
-							position[3] = test % n;
-							numSolution += queen(position, 4);
-						}
+			set_limit(limit, 0, test / n / n / n, 1);
+			if(printed++ == 0)print_limit(limit, n);
+			if(!limit[1][test / n / n % n]){
+				set_limit(limit, 1, test / n / n % n, 1);
+				if(!limit[2][test / n % n]){
+					set_limit(limit, 2, test / n % n, 1);
+					if(!limit[3][test % n]){
+						set_limit(limit, 3, test % n, 1);
+						numSolution += queen(limit, 4);
+						set_limit(limit, 3, test % n, -1);
 					}
+					set_limit(limit, 2, test / n % n, -1);
 				}
+				set_limit(limit, 1, test / n / n % n, -1);
 			}
+			set_limit(limit, 0, test / n / n / n, -1);
+			if(printed++ == 1)print_limit(limit, n);
 		}
 		printf("Case %d: %d\n", ++case_num, numSolution);
 	}
