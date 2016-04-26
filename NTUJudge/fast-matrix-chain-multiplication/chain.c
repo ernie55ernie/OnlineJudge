564f8bfa9e687c65362989b76b94599698f7a472
Yu-Wei_Chang <ernie55ernie@yahoo.com.tw>
2016-04-27 05:36:35 +0800

    Remain one test data

--- NTUJudge/fast-matrix-chain-multiplication/chain.c
+++ NTUJudge/fast-matrix-chain-multiplication/chain.c
@@ -28,57 +28,96 @@ uint32_t signature(uint32_t *A, int r, int c) {
     return h;
 }
 
-void multiply(int N, int M, int O, uint32_t A[], uint32_t B[], uint32_t C[]) {
+uint32_t* multiply(int N, int M, int O, uint32_t* A, uint32_t* B) {
+    uint32_t* C = (uint32_t*)malloc(sizeof(uint32_t) * N * O);
 #pragma omp parallel for
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < O; j++) {
             uint32_t sum = 0;    // overflow, let it go.
             for (int k = 0; k < M; k++)
-                sum += A[i * M + k] * B[k * M + j];
-            C[i * M + j] = sum;
+                sum += A[i * M + k] * B[k * O + j];
+            C[i * O + j] = sum;
         }
+    }
+    free(A);
+    free(B);
+    return C;
+}
+
+uint32_t* mult(uint32_t* matrix[], int* trace[], int Z[], int i, int j) {
+    if (i == j){
+        // printf("%d\n", i);
+        return matrix[i];
+    }else{
+        int k = trace[i + 1][j + 1];
+        uint32_t* Y = mult(matrix, trace, Z, k, j);     // Y=A[k..j]
+        uint32_t* X = mult(matrix, trace, Z, i, k - 1);          // X=A[i..k-1]
+        // printf("%d %d %d ", i, j, k);
+        // printf("%d %d %d %d %d %d\n", i, k, j + 1,
+        //     Z[i], Z[k], Z[j + 1]);
+        return multiply(Z[i], Z[k], Z[j + 1], X, Y);
     }
 }
 
-int matrixchain(int Z[], int trace[], int n)
+void matrixchain(int Z[], int* trace[], int N)
 {
-    int m[n][n];
  
-    int i, j, k, L, q, l;
- 
+    int i, j, k, L;
+
+    uint64_t table[N + 1][N + 1];
+
+    uint64_t q;
+
     /*cost is zero when multiplying one matrix.*/
-    for (i = 1; i < n; i++)
-        m[i][i] = 0;
+    for (i = 1; i <= N; i++)
+        table[i][i] = 0;
  
     /*L is chain length. */
-    for (L = 2; L < n; L++)   
+    for (L = 2; L <= N; L++)   
     {
-        for (i = 1; i <= (n - L + 1); i++)
+        for (i = 1; i <= (N - L + 1); i++)
         {
             j = i + L - 1;
-            m[i][j] = INT_MAX;
-            for (k = i; k<= j - 1; k++)
+            table[i][j] = INT_MAX;
+            for (k = i; k <= j - 1; k++)
             {
                 // q = cost/scalar multiplications
-                q = m[i][k] + m[k + 1][j] + Z[i - 1] * Z[k] * Z[j];
-                if (q < m[i][j]){
-                    m[i][j] = q;
+                q = table[i][k] + table[k + 1][j] + (int64_t)Z[i - 1] * Z[k] * Z[j];
+                if (q < table[i][j]){
+                    table[i][j] = q;
+                    trace[i][j] = k;
                 }
             }
         }
     }
-    return m[1][n - 1];
+    // for(i = 1; i < N + 1; i++){
+    //     for(j = 1; j < N + 1; j++){
+    //         printf("%10llu ", j < i ? 0 : table[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 }
 
 void calculate(int Z[], uint32_t* matrix[], int N){
-    int i;
-    int trace[N];
-    int min = matrixchain(Z, trace, N);
-    // matrix[N] = (uint32_t *) malloc(sizeof(uint32_t) * Z[0] * Z[N]);
-    printf("%d\n", min);
-    for(i = 0; i < N; i++){
-        // printf("%d ", trace[i]);
+    int i, j;
+    int* trace[N + 1];
+    for(i = 0; i < N + 1; i++){
+        trace[i] = (int*)malloc(sizeof(int*) * (N + 1));
+        for(j = 0; j < N + 1; j++){
+            trace[i][j] = 0;
+        }
     }
+    matrixchain(Z, trace, N);
+    // printf("\n");
+    // for(i = 1; i < N + 1; i++){
+    //     for(j = 1; j < N + 1; j++){
+    //         printf("%10d ", trace[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    matrix[N] = mult(matrix, trace, Z, 0, N - 1);
+    for(i = 0; i < N + 1; i++)
+        free(trace[i]);
 }
 
 int main()
@@ -93,10 +132,17 @@ int main()
         }
         for(i = 0; i < N; i++){
             scanf("%d", &S);
+            // printf("%d %d %d\n", S, Z[i], Z[i + 1]);
             matrix[i] = rand_gen(S, Z[i], Z[i + 1]);
         }
         calculate(Z, matrix, N);
-        printf("%d\n", signature(matrix[N], Z[0], Z[N]));
+        // matrix[N] = multiply(Z[0], Z[1], Z[2], matrix[0], matrix[1]);
+        // for(int i = 2; i < N; i++){
+        //     matrix[N] = multiply(Z[0], Z[i], Z[i + 1], matrix[N], matrix[i]);
+        // }
+        // printf("%d %d ", Z[0], Z[N]);
+        printf("%u\n", signature(matrix[N], Z[0], Z[N]));
+        free(matrix[N]);
     }
     return 0;
 }
